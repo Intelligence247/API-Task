5464e5a32fb47a244f1d782a687c3b236a0e50ab
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-02-24 22:00:29 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import { Loading } from './Components/Loading'
 
 export default function App() {
   const [pic, setPic] = useState([])
-  const [navigate, setNavigate] = useState(0)
+  const [navigate, setNavigate] = useState(99)
+  const [color, setColor] = useState('#000000')
+  const [color2, setColor2] = useState('#fff')
 const url = 'https://api.imgflip.com/get_memes'
 
 const fetchAPI=()=>{
@@ -19,33 +21,47 @@ const fetchAPI=()=>{
 }, [])
 
 const handleAdd=()=>{
+  const randC= "#"+Math.random().toString(16).substr(2,6)
+  const randC2= "#"+Math.random().toString(16).substr(2,6)
+  setColor(randC)
+  setColor2(randC2)
+  console.log(randC)
+
   setNavigate(navigate+1)
   if(navigate === pic.length-1){
     setNavigate(0)
-  }
+     }
 }
 const handleMinus=()=>{
+  const randC= "#"+Math.random().toString(16).substr(2,6)
+  setColor(randC)
+  const randC3= "#"+Math.random().toString(16).substr(2,6)
+setColor2(randC3)
   setNavigate(navigate-1)
   if(navigate===0){
     setNavigate(pic.length-1)
+    
   }
-  // setNavigate(9)
 }
 const rand= Math.floor(Math.random()*pic.length)
   return (
     <div className="App">
-      
- {pic.length == 0 ?
+ {pic.length === 0 ?
 <Loading/>
      :(
-     <section className=''>
+       <section className=''>
+      <h1 style={{background:`linear-gradient(135deg, ${color2}, ${color})`, animation:' filter 2s infinite linear'}} >Are you a memester? We can assure you of getting the best memes here!!!</h1>
+        <div className="flex justify-center gap-4">
+   <button onClick={handleAdd}><span></span></button>
       <div className="imgWrapper">
+        <div className="imgs">
    <img src={pic[navigate].url} alt="" />
-   <figcaption>{pic[navigate].name}</figcaption>
-   <button onClick={handleAdd}>To</button>
-   <button onClick={handleMinus}>Fro</button>
    </div>
-   
+   <figcaption style={{color:`${color}`}}>{pic[navigate].name}</figcaption>
+   </div>
+  
+   <button onClick={handleMinus}><span></span></button>
+   </div>
      </section>
       )
 }
